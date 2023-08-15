5d9983cbe9ab7373001ec94211e9230ae9d18a88
R0flan4k <klexak05@gmail.com>
2023-08-15 20:51:24 +0300

    finalAlphaVer

--- alphaVER.cpp
+++ alphaVER.cpp
@@ -13,49 +13,49 @@ coefs get_coefs(void);
 int show_ans(const coefs *);
 int main(void)
 {
-    coefs input;
+    coefs input = {0,0,0};
     show_menu();
     input=get_coefs();
     show_ans(&input);
-    puts("Vsego dobrogo! Programma zavershena.");
+    puts("Have a good day! End.");
     return 0;
 }
 void show_menu(void)
 {
-    puts("Dobriy den, Ruslan Alekseyevich.");
-    puts("Eta programma prednoznachena dlya reshenya kvadratnih yravneniy");
-    puts("Pust' yravneniye imeet vid: ax^2 + bx + c = 0.");
+    puts("Hello, Ruslan Alekseyevich.");
+    puts("This program can solve quadratic equation");
+    puts("Equation must looks like: ax^2 + bx + c = 0.");
 }
 coefs get_coefs(void)
 {
-    coefs input;
-    puts("Vvedite koeficienti a,b,c:");
+    coefs input = {0,0,0};
+    puts("Enter coeficients a,b,c:");
     while (scanf("%f%f%f",&input.a,&input.b,&input.c)!=3)
     {
-        puts("Oshibka. Vvedite 3 deysvitelnih chisla,");
-        puts("takih kak 12, -1.5, 3.14 ili 1.618");
+        puts("Error. Enter 3 real numbers");
+        puts("like 12, -1.5, 3.14 or 1.618");
         scanf("%*s");
     }
     return input;
 }
 int show_ans(const coefs * inp)
 {
-    printf("Vvedennye uravneniye: %.3g*x^2 + %.3g*x + %.3g = 0\n",inp->a,inp->b,inp->c);
+    printf("Your equation: %.3g*x^2 + %.3g*x + %.3g = 0\n",inp->a,inp->b,inp->c);
     if (DISCR(inp->a,inp->b,inp->c)<0)
     {
-        puts("Dannoye yravneniye ne imeet veshestvennih korney.");
+        puts("This equation hasn't real roots.");
         return 0;
     }
     else if (DISCR(inp->a,inp->b,inp->c)==0)
     {
-        puts("Dannoue yravneniye yavlayetca polnim kvadratom (discriminant = 0)");
-        printf("koren yravneniya: %5.3g\n",FIRST_ROOT(inp->a,inp->b,inp->c));
+        puts("This equation has only one root (discriminant == 0)");
+        printf("This root: %5.3g\n",FIRST_ROOT(inp->a,inp->b,inp->c));
         return 1;
     }
     else
     {
-        puts("Dannoye yravneniye imeet 2 kornya");
-        printf("perviy koren: %5.3g\nvtoroy koren: %5.3g\n",FIRST_ROOT(inp->a,inp->b,inp->c),
+        puts("This equation has 2 roots");
+        printf("first root: %5.3g\nsecond root: %5.3g\n",FIRST_ROOT(inp->a,inp->b,inp->c),
         SECOND_ROOT(inp->a,inp->b,inp->c));
         return 2;
     }
