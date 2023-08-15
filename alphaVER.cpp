c6bb6626a8f32c17d49327bfe95341fec7061f47
R0flan4k <klexak05@gmail.com>
2023-08-15 19:35:18 +0300

    Update alphaVER.cpp

--- alphaVER.cpp
+++ alphaVER.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
-#include <math.h>
+#include <tgmath.h>
+#define DISCR(A,B,C) (pow(B,2.0)-4*(A)*(C))
+#define FIRST_ROOT(A,B,C) ((-(B)-sqrt((pow(B,2.0))-4*(A)*(C)))/(2*(A)))
+#define SECOND_ROOT(A,B,C) ((-(B)+sqrt((pow(B,2.0))-4*(A)*(C)))/(2*(A)))
 typedef struct {
     float a;
     float b;
@@ -10,17 +13,18 @@ coefs get_coefs(void);
 void show_ans(const coefs *);
 int main(void)
 {
-    coefs inpt;
+    coefs input;
     show_menu();
-    inpt=get_coefs();
-    printf("%3.3g %3.3g %3.3g\n",inpt.a,inpt.b,inpt.c);
+    input=get_coefs();
+    printf("%3.3g %3.3g %3.3g\n",DISCR(input.a,input.b,input.c),FIRST_ROOT(input.a,input.b,input.c),
+    SECOND_ROOT(input.a,input.b,input.c));
     return 0;
 }
 void show_menu(void)
 {
     puts("Dobriy den, Ruslan Alekseyevich.");
     puts("Eta programma prednoznachena dlya reshenya kvadratnih yravneniy");
-    puts("Pust' yravneniye imeet vid ax^2+bx+c=0.");
+    puts("Pust' yravneniye imeet vid: ax^2 + bx + c = 0.");
 }
 coefs get_coefs(void)
 {
@@ -29,8 +33,12 @@ coefs get_coefs(void)
     while (scanf("%f%f%f",&input.a,&input.b,&input.c)!=3)
     {
         puts("Oshibka. Vvedite 3 deysvitelnih chisla,");
-        puts("takih kak 12, 1.5, 3.14 ili 1.618");
+        puts("takih kak 12, -1.5, 3.14 ili 1.618");
         scanf("%*s");
     }
     return input;
+}
+void show_ans(const coefs * inp)
+{
+    
 }
